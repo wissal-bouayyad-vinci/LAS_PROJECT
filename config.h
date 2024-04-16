7862dd04fbcdc242e91e06b8d390771cdc3561b2
Ubuntu <studinfo@lab000001.f10lcdcgbu3e1abtmcu3refnug.ax.internal.cloudapp.net>
2024-04-16 15:46:46 +0200

    added file config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//CLÉS DE MÉMOIRE PARTAGÉE ET SÉMAPHORES 
+#define SHM_KEY 248
+#define SEM_KEY 369
+#define PERM 0666
+
+//PORT SERVEUR ET ADRESSE IP
+#define SERVER_PORT 9090
+#define SERVER_IP "127.0.0.1" /* localhost */
+
+#endif
+
