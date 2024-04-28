45fe4eb67ea1c333bff1696ca02ae7dc8201e87e
wissal-bouayyad-vinci <129200478+wissal-bouayyad-vinci@users.noreply.github.com>
2024-04-28 20:08:08 +0200

    Update client.c

--- client.c
+++ client.c
@@ -152,27 +152,25 @@ int main(int argc, char const *argv[])
         // on a fini le jeux 
         int scoreFinal = scoreCalculation(grille);
         int score=scoreFinal;
-        ret = swrite(sockfd,&score,sizeof(int));
+        int nbPlayers;
+        swrite(sockfd,&score,sizeof(int));
+        sread(sockfd, &nbPlayers, sizeof(int));
+        sread(sockfd,&message,sizeof(message));
+
+        if ((message.code==RANKING)){
+             Structplayer* players;
+             sread(sockfd,&players,sizeof(players));
+             printRanking(players,size);
+             free(players);
+         }else{
+              printf("Réponse Serveur: non prevue %d.\n", message.code);                
+          }
             
-        while(sread(sockfd,&message,sizeof(message))){
-
-                if ((message.code==RANKING)){
-                    struct Structplayer* players;
-                    int size;   
-                    sread(sockfd,&players,sizeof(players));
-                    sread(sockfd,&size,sizeof(size));
-                    printRanking(players,size);
-                    free(players);
-
-                }else{
-                    printf("Réponse Serveur: non prevue %d.\n", message.code);                
-                }
-            }
  
-            //  end 
-            printf("END GAME\n");
-            sclose(sockfd);
-            free(grille);
+          //  end 
+          printf("END GAME\n");
+          sclose(sockfd);
+          free(grille);
         
     }else{
 
